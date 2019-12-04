9bdb6464f7fec48855a0bdf68b7d7b448dc3f73d
PopeyesBiscuit <nathanielveldkamp@gmail.com>
2019-12-04 14:48:36 +0100

    Programma frame

--- ASDdesktop/src/View/Main.java
+++ ASDdesktop/src/View/Main.java
@@ -2,17 +2,27 @@ package View;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 
 public class Main extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		launch(args);
 
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+		Pane root = new MainScreen();
+		Scene scene = new Scene(root, 700,700);
+		
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Boebot interface");
+		primaryStage.show();
 		
 	}
 
---
+++ ASDdesktop/src/View/MainScreen.java
@@ -0,0 +1,7 @@
+package View;
+
+import javafx.scene.layout.Pane;
+
+public class MainScreen extends Pane {
+
+}
